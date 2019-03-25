8c827a1aebcdb36c67c7a9965a8733632e6fb61f
ismailfaruk <ismailfaruk.17@gmail.com>
2019-03-25 10:03:53 -0400

    File name changed

--- Team3_Final_Project/GenerateSineWave.c
+++ Team3_Final_Project/GenerateSineWave.c
@@ -20,25 +20,19 @@ References Used
 //define an array
 //instantiate the array
 */
+#include "generateSineWave.h"
 
-/*-----------------library inlcuded------------------*/
-#include "arm_math.h"
-
-/*--------------------Private function prototypes -----------------*/
-void generateSineWave(float_32 frequency1, float_32 frequency2);
-void signalMixer(float_32 *mixCoef);
-/*-----------------------------------------------------------------*/
+//extern UART_HandleTypeDef huart1;
 
 /*-------------User Global Variables----------------------------*/
+/*------------------External variables -----------------*/
+float32_t frequencyVal[2] = {0.0,0.0};	//frequency values
+float32_t mixerVal[4] = {0.0,0.0,0.0,0.0};	//mixer values
 
-//to be changed to float32_t
 
 #define SamplingFrequency 16000		//16000 Hz
 #define SignalTime 2				//2 seconds
 #define SampleSize SamplingFrequency*SignalTime
-
-//User input values
-//
 
 //Signal arrays
 float32_t signal1Array[SampleSize];	//most probably will not need
@@ -48,13 +42,13 @@ float32_t signalContainer[2][SampleSize];	//consider using 2-D array
 //Mixed Signal arrays
 float32_t mixedSignal1[SampleSize];	//most probably will not need
 float32_t mixedSignal2[SampleSize];	//most probably will not need
-float32_t mixerArray[2][2];	//consider using 2-D array 
+float32_t mixerArray[2][2];	//consider using 2-D array
 float32_t mixedSignalContainer[2][SampleSize];	//consider using 2-D array 
 
 //array definitions
 float32_t signal_f32[SampleSize*2];
 float32_t mixer_f32[4];
-float32_t mixedSignal_f32[SampleSize*2]
+float32_t mixedSignal_f32[SampleSize*2];
 
 //array instances
 arm_matrix_instance_f32 signal;
@@ -62,45 +56,41 @@ arm_matrix_instance_f32 mixer;
 arm_matrix_instance_f32 mixedSignal;
 
 
-// Enumerator for system state
-enum systemOption{None, GenerateWaves, MixWaves, PlayUnmixed, PlayMixed} optionState;
-enum inputState{OK, NOT_OK};
-
 int main (void){
 
 	//General UI function call
 	userInterfaceWelcome();
 
 	/*-------------------------Inside While Loop-----------------------------------*/
+	while(1){
+		userInterfaceOptions();
+			
+		if (optionState == GenerateWaves){
+			//call UI to input frequency values
+			userInterfaceGenerateSignal();
+			//generateSineWave();
+			//store to SRAM
+		}
 
-	//System options
-	char[] optionstate = userInterfaceOptions();
+		if (optionState == MixWaves){
+			//call UI to input mixer values
+			userInterfaceMixer();
+			//signalMixer
+			//store to SRAM
+		}
 
-	if (optionState == GenerateWaves){
-		//UI for input of frequency value
-		userInterafaceFrequency();
-		//take input for frequency from UART
-		generateSineWave(frequency1, frequency2);
-		//store to SRAM
+		if (optionState == PlayUnmixed){
+			//DAC(s1);
+			//DAC(s2);
+		}
+
+		if (optionState == PlayMixed){
+			//DAC(x1);
+			//DAC(x2);
+		}
+
 	}
-
-	if (optionState == MixWaves){
-		//UI for input of mixer values
-		userInterfaceMixer();
-		//take input for mixerArray from UART
-		//store to SRAM
-	}
-
-	if (optionState == PlayUnmixed){
-		//DAC(s1);
-		//DAC(s2);
-	}
-
-	if (optionState == PlayMixed){
-		//DAC(x1);
-		//DAC(x2);
-	}
-
+	/*----------------------------------------------------------------------------*/
 }
 
 /*------------------------------------------UI function to show welcome message --------------------------------------------*/
@@ -108,11 +98,15 @@ void userInterfaceWelcome(void){
 	/*-----------------------------------------------------Output Message Arrays---------------------------------------------------------------*/
 
 	//Welcome Messages
-	char welcomeMessage[] = {'\n','W','e','l','c','o','m','e',' ','t','o',' ',
-							'D','I','Y',' ','S','o','u','n','d',' ','G','e','n',
-							'e','r','a','t','o','r',' ','2','0','1','9',' '};
+	char welcomeMessage[41] = {'\n','-','-','-','-','W','e','l','c','o','m','e',' ','t','o',' ',
+														'D','I','Y',' ','S','o','u','n','d',' ','G','e','n',
+														'e','r','a','t','o','r',' ','2','0','1','9',' '};
 
-	char WelcomeMessage2[] = {'A','K','A',' ','E','C','S','E','4','4','4','\n'};
+	char welcomeMessage2[16] = {'A','K','A',' ','E','C','S','E','4','4','4','-','-','-','-','\n'};
+	
+	HAL_UART_Transmit(&huart1, (uint8_t *)welcomeMessage, 41, 30000);
+	HAL_UART_Transmit(&huart1, (uint8_t *)welcomeMessage2, 16, 30000);
+
 	/*-----------------------------------------------------------------------------------------------------------------------------------------*/
 }
 /*--------------------------------------------------------------------------------------------------------------------------*/
@@ -124,18 +118,18 @@ void userInterfaceOptions(void){
 	/*------------------------------------------Output Message Arrays------------------------------------------*/	
 
 	//Asking for system function, i.e. what should the the system do
-	char systemStateMessage[] = {'\n','W','h','a','t',' ','w','o','u','l','d',' ','y','o',
-								'u',' ','l','i','k','e',' ','t','o',' ','y','o','u','?','\n'};
+	char systemStateMessage[29] = {'\n','W','h','a','t',' ','w','o','u','l','d',' ','y','o',
+																'u',' ','l','i','k','e',' ','t','o',' ','y','o','u','?','\n'};
 	
 	//Showing input format and example for option values
-	char optionsExampleMessage[] = {'1',' ','G','e','n','e','r','a','t','e',' ','S','i','g','n','a','l','s','\n',
-							'2',' ','M','i','x',' ','S','i','g','n','a','l','s','\n',
-							'3',' ','P','l','a','y',' ','U','n','m','i','x','e','d',' ','S','i','g','n','a','l','s','\n',
-							'4',' ','P','l','a','y',' ','M','i','x','e','d',' ','S','i','g','n','a','l','s','\n',
-							'I','n','p','u','t',' ','o','p','t','i','o','n',' ','n','u','m','b','e','r',' ','o','n','l','y','\n'};
+	char optionsExampleMessage[103] = {'1',' ','G','e','n','e','r','a','t','e',' ','S','i','g','n','a','l','s','\n',
+																		'2',' ','M','i','x',' ','S','i','g','n','a','l','s','\n',
+																		'3',' ','P','l','a','y',' ','U','n','m','i','x','e','d',' ','S','i','g','n','a','l','s','\n',
+																		'4',' ','P','l','a','y',' ','M','i','x','e','d',' ','S','i','g','n','a','l','s','\n','\n',
+																		'I','n','p','u','t',' ','o','p','t','i','o','n',' ','n','u','m','b','e','r',' ','o','n','l','y','\n'};
 
 	//Showing the label for option value being input
-	char optionVarMessage[] = {'O','p','t','i','o','n',':',' '};
+	char optionVarMessage[8] = {'O','p','t','i','o','n',':',' '};
 
 	/*--------------------------------------------------------------------------------------------------------*/
 	#pragma endregion OutputMessageArrays
@@ -160,22 +154,21 @@ void userInterfaceOptions(void){
 
 		/*-----------Transmits generic explanation and example for frequency to user-------------*/
 		
-		HAL_UART_Transmit(&huart1, (uint8_t *)systemStateMessage, 18, 30000);
-		HAL_UART_Transmit(&huart1, (uint8_t *)optionsExampleMessage, 66, 30000);
+		HAL_UART_Transmit(&huart1, (uint8_t *)systemStateMessage, 29, 30000);
+		HAL_UART_Transmit(&huart1, (uint8_t *)optionsExampleMessage, 103, 30000);
 
 		/*---------------------------------------------------------------------------------------*/
 
 		/*-------------------------Receivies and transmits frequency values----------------------*/
 		
-		HAL_UART_Transmit(&huart1, (uint8_t *)optionVarMessage 12, 30000);
+		HAL_UART_Transmit(&huart1, (uint8_t *)optionVarMessage, 8, 30000);
 		while(HAL_UART_Receive (&huart1, (uint8_t *)&inputOption, 1, 30000) != HAL_OK){}
 		HAL_UART_Transmit(&huart1, (uint8_t *)&inputOption[0], 1, 30000);
 			
 		/*---------------------------------------------------------------------------------------*/
 
-		//Call function to verify input
-		int returnValue = isCorrect();
-		if (returnValue = 1) {
+		//Function call to verify input
+		if (isCorrect()) {
 			systemOption = OK;
 		}
 
@@ -183,6 +176,7 @@ void userInterfaceOptions(void){
 	/*-------------------------------------------------------------------------------------------------------*/
 	#pragma endregion USART_Implementation
 
+	#pragma region StateChange
 	/*-------------------------------------Setting optionState to User input----------------------------------*/
 
 	if (inputOption[0] == '1'){
@@ -203,6 +197,7 @@ void userInterfaceOptions(void){
 	}
 		
 	/*--------------------------------------------------------------------------------------------------------*/
+	#pragma endregion StateChange
 }
 /*----------------------------------------------------------------------------------------------------------------------------*/
 
@@ -213,8 +208,8 @@ void userInterfaceGenerateSignal(){
 	/*------------------------------------------Output Message Arrays------------------------------------------*/	
 	
 	//Function Introduction 
-	char frequencyValuesMessage[18] = {'\n','S','i','g','n','a','l',' ',
-															'F','r','e','q','u','e','n','c','y','\n'};
+	char frequencyValuesMessage[34] = {'\n','-','-','-','-','-','-','-','-','S','i','g','n','a','l',' ',
+															'F','r','e','q','u','e','n','c','y','-','-','-','-','-','-','-','-','\n'};
 
 	//Showing input format and example for frequency values
 	char frequencyExampleMessage[66] = {'\n','E','n','t','e','r',' ','t','o','t','a','l',
@@ -256,7 +251,8 @@ void userInterfaceGenerateSignal(){
 
 		/*-----------Transmits generic explanation and example for frequency to user--------------*/
 		
-		HAL_UART_Transmit(&huart1, (uint8_t *)frequencyValuesMessage, 18, 30000);
+		
+		HAL_UART_Transmit(&huart1, (uint8_t *)frequencyValuesMessage, 34, 30000);
 		HAL_UART_Transmit(&huart1, (uint8_t *)frequencyExampleMessage, 66, 30000);
 
 		/*-----------------------------------------------------------------------------------------*/
@@ -276,20 +272,31 @@ void userInterfaceGenerateSignal(){
 
 		/*---------------------------------------------------------------------------------------*/
 
-		/*------------------------Converting Frequency Char to Flag values------------------------*/
-
-		
-
-		/*----------------------------------------------------------------------------------------*/
-
-		//Call function to verify input
-		int returnValue = isCorrect();
-		if (returnValue = 1) {
+		//Function call to verify input
+		if (isCorrect()) {
 			freqFlag = OK;
 		}
 	}
 	/*-------------------------------------------------------------------------------------------------------*/
 	#pragma endregion USART_Implementation
+
+	#pragma region CharToFlagConvert
+	/*------------------------Converting Frequency Char to Float values------------------------*/
+
+	int i;
+	int j;
+	for(i=0; i<2; i++){
+		for(j=5; j>=0;j--){
+			if(inputFreq[i][j] == '.'){}
+			else{
+				frequencyVal[i] = frequencyVal[i] + (float32_t)inputFreq[i][j]*10;
+			}
+		}
+		frequencyVal[i] = frequencyVal[i]/1000;
+	}
+
+	/*----------------------------------------------------------------------------------------*/
+	#pragma endregion
 }
 /*----------------------------------------------------------------------------------------------------------------------------*/
 
@@ -300,17 +307,26 @@ void userInterfaceMixer(void){
 	/*------------------------------------------Output Message Arrays------------------------------------------*/
 
 	//Function Introduction
-	char linearCoefficientsMessage[33] = {'\n','L','i','n','e','a','r',' ','C','o','m','b',
-																'i','n','a','t','i','o','n',' ','C','o','e','f','f',
-																'i','c','i','e','n','t','s','\n'};
+	char linearCoefficientsMessage[49] = {'\n','-','-','-','-','-','-','-','-','L','i','n','e','a','r',' ','C','o','m','b',
+																			'i','n','a','t','i','o','n',' ','C','o','e','f','f',
+																			'i','c','i','e','n','t','s','-','-','-','-','-','-','-','-','\n'};
 	
+	//Showing placement of mixer values in matrix
+	char mixerMatrix[198] = {'\n',' ','_',' ',' ','_',' ',' ',' ',' ',' ','_','_',' ',' ',' ',' ',' ',' ',' ',' ','_','_',' ',' ',' ',' ','_',' ',' ','_','\n',
+												'|',' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ','|',' ',' ',' ',' ','|',' ','\n',
+												'|',' ','x','1',' ','|',' ',' ',' ','|',' ',' ','a','0','0',' ',' ','a','1','2',' ',' ','|',' ',' ','|',' ','s','1',' ','|',' ','\n',
+												'|',' ',' ',' ',' ','|',' ','=',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ','|',' ',' ',' ',' ','|',' ','\n',
+												'|',' ','x','2',' ','|',' ',' ',' ','|',' ',' ','a','2','1',' ',' ','a','1','1',' ',' ','|',' ',' ','|',' ','s','2',' ','|',' ','\n',
+												'|','_',' ',' ','_','|',' ',' ',' ','|','_','_',' ',' ',' ',' ',' ',' ',' ',' ','_','_','|',' ',' ','|','_',' ',' ','_','|',' ','\n','\n'};
+																																							
 	//Showing input format and example for mixer values
-	char mixerExampleMessage[65] = {'E','n','t','e','r',' ','t','o','t','a','l',' ',
+	char mixerExampleMessage[66] = {'\n','E','n','t','e','r',' ','t','o','t','a','l',' ',
 																	'4',' ','d','i','g','i','t','s',',',' ','w','i','t',
 																	'h',' ','2',' ','d','e','c','i','m','a','l',' ','p','l',
 																	'a','c','e','s',' ','a','c','c','u','r','a','c','y',
 																	'\n','L','i','k','e',' ','5','5','.','7','7','\n'};
-	
+
+
 	//Showing the label for frequency values being input
 	char mixerVarMessage[4][13] = {
 																{'\n','E','n','t','e','r',' ','a','0','0', ' ', ':', ' '},
@@ -344,11 +360,13 @@ void userInterfaceMixer(void){
 	mixerFlag = NOT_OK;
 
 	while(mixerFlag == NOT_OK){
+		
 		/*---------------Transmits generic explanation and example for matrix coefficients to user--------------*/
 		
-		HAL_UART_Transmit(&huart1, (uint8_t *)linearCoefficientsMessage, 33, 30000);
-		HAL_UART_Transmit(&huart1, (uint8_t *)mixerExampleMessage, 65, 30000);
-
+		HAL_UART_Transmit(&huart1, (uint8_t *)linearCoefficientsMessage, 49, 30000);
+		HAL_UART_Transmit(&huart1, (uint8_t *)mixerMatrix, 198, 30000);
+		HAL_UART_Transmit(&huart1, (uint8_t *)mixerExampleMessage, 66, 30000);
+		
 		/*------------------------------------------------------------------------------------------------------*/
 
 		/*-----------------------------Receivies and transmits matrix coefficient values---------------------------*/
@@ -366,13 +384,31 @@ void userInterfaceMixer(void){
 
 		/*---------------------------------------------------------------------------------------------------------*/
 		
-		int returnValue = isCorrect();
-		if (returnValue = 1) {
+		//Function call to verify input
+		if (isCorrect()) {
 			mixerFlag = OK;
 		}
 	}
 	/*------------------------------------------------------------------------------------------------------------------------*/
 	#pragma endregion USART_Implementation
+
+	#pragma region CharToFlagConvert
+	/*------------------------Converting Frequency Char to Float values------------------------*/
+
+	int i;
+	int j;
+	for(i=0; i<4; i++){
+		for(j=4; j>=0;j--){
+			if(inputCoeff[i][j] == '.'){}
+			else{
+				mixerVal[i] = mixerVal[i] + (float32_t)inputCoeff[i][j]*10;
+			}
+		mixerVal[i] = mixerVal[i]/1000;
+		}
+	}
+
+	/*----------------------------------------------------------------------------------------*/
+	#pragma endregion
 }
 /*----------------------------------------------------------------------------------------------------------------------------*/
 
@@ -383,7 +419,7 @@ int isCorrect(){
 	/*------------------------------------------Output Message Arrays------------------------------------------*/
 	
 	//Confimation query
-	char toConfirmMessage[42] = {'\n','A','r','e',' ','y','o','u',' ','s','u','r','e',
+	char toConfirmMessage[43] = {'\n','\n','A','r','e',' ','y','o','u',' ','s','u','r','e',
 															' ','t','h','e',' ','i','n','p','u','t',' ','i','s',
 															' ','c','o','r','r','e','c','t','?','(','y','/','n',')'};
 	
@@ -415,7 +451,7 @@ int isCorrect(){
 	//Polling the user to get a desired response
 	while(1){
 		//Ask if input frequency values are correct
-		HAL_UART_Transmit(&huart1, (uint8_t *)toConfirmMessage, 42, 30000);
+		HAL_UART_Transmit(&huart1, (uint8_t *)toConfirmMessage, 43, 30000);
 		
 		//Wait for response
 		while(HAL_UART_Receive(&huart1, (uint8_t *)comfirmInput, 1, 30000) != HAL_OK){}
@@ -451,26 +487,26 @@ int isCorrect(){
 }
 /*---------------------------------------------------------------------------------------------------------*/
 
+
 // Generates two signals based on the frequency given
-void generateSineWave(float32_t signalFrequency1, float32_t signalFrequency2){
-	signalFrequency1 = 440;
+void generateSineWave(void){
 	int sample;
 	//generate samples worth 2 seconds of a sine wave
 	for (sample = 0; sample < SampleSize; sample++){
         
 		/*-----------------------signal generation using 2-D array---------------------------*/
-		signalContainer[0][sample] = arm_sin_f32(2*signalFrequency1*sample/SamplingFrequency);     //Signal1
-		signalContainer[1][sample] = arm_sin_f32(2*signalFrequency2*sample/SamplingFrequency);     //Signal2
+		signalContainer[0][sample] = arm_sin_f32(2*frequencyVal[0]*sample/SamplingFrequency);     //Signal1
+		signalContainer[1][sample] = arm_sin_f32(2*frequencyVal[1]*sample/SamplingFrequency);     //Signal2
 		/*-----------------------------------------------------------------------------------*/
 
 		/*-----------------------signal generation using 1-D array---------------------------*/
-		signal1Array[sample] = arm_sin_f32(2*signalFrequency1*sample/SamplingFrequency);	//Signal1
-		signal2Array[sample] = arm_sin_f32(2*signalFrequency2*sample/SamplingFrequency);	//Signal2
+		signal1Array[sample] = arm_sin_f32(2*frequencyVal[0]*sample/SamplingFrequency);	//Signal1
+		signal2Array[sample] = arm_sin_f32(2*frequencyVal[1]*sample/SamplingFrequency);	//Signal2
 		/*-----------------------------------------------------------------------------------*/
 
 		/*-----------------------signal generation to be used in ARM matrix instances---------------------------*/
-		signal_f32[sample] = arm_sin_f32(2*signalFrequency1*sample/SamplingFrequency);     //Signal1
-		signal_f32[sample+SampleSize] = arm_sin_f32(2*signalFrequency2*sample/SamplingFrequency);     //Signal2
+		signal_f32[sample] = arm_sin_f32(2*frequencyVal[0]*sample/SamplingFrequency);     //Signal1
+		signal_f32[sample+SampleSize] = arm_sin_f32(2*frequencyVal[1]*sample/SamplingFrequency);     //Signal2
 		/*-----------------------------------------------------------------------------------*/
 	}	
 }
@@ -493,9 +529,7 @@ void signalMixer(void){
 	int columns = 2;
 	arm_mat_init_f32(&mixer, rows, columns, mixer_f32);
 	
-	
-	int rows = 2;	//number of sign waves
-  	int columns = SampleSize;
+  	columns = SampleSize;
 	arm_mat_init_f32(&signal, rows, columns, (float32_t *)signal_f32);
 	arm_mat_init_f32(&mixedSignal, rows, columns, (float32_t *)mixedSignal_f32);
 
--- Team3_Final_Project/GenerateSineWave.h
+++ Team3_Final_Project/GenerateSineWave.h
@@ -3,48 +3,22 @@
 
 /*-----------------library inlcuded------------------*/
 #include "arm_math.h"
-
-/*-------------User Global Variables----------------------------*/
-
-#define SamplingFrequency 16000		//16000 Hz
-#define SignalTime 2				//2 seconds
-#define SampleSize SamplingFrequency*SignalTime
-
-//User input values
-float32_t frequencyVal[2] = {0.0,0.0};	//frequency values
-float32_t mixerVal[4] = {0.0,0.0,0.0,0.0};	//mixer values
-
-//Signal arrays
-float32_t signal1Array[SampleSize];	//most probably will not need
-float32_t signal2Array[SampleSize];	//most probably will not need
-float32_t signalContainer[2][SampleSize];	//consider using 2-D array
-
-//Mixed Signal arrays
-float32_t mixedSignal1[SampleSize];	//most probably will not need
-float32_t mixedSignal2[SampleSize];	//most probably will not need
-float32_t mixerArray[2][2];	//consider using 2-D array
-float32_t mixedSignalContainer[2][SampleSize];	//consider using 2-D array 
-
-//array definitions
-float32_t signal_f32[SampleSize*2];
-float32_t mixer_f32[4];
-float32_t mixedSignal_f32[SampleSize*2];
-
-//array instances
-arm_matrix_instance_f32 signal;
-arm_matrix_instance_f32 mixer;
-arm_matrix_instance_f32 mixedSignal;
+#include "stm32l4xx_hal.h"
 
 //Enumerator for system state
-enum systemOption{None, GenerateWaves, MixWaves, PlayUnmixed, PlayMixed} optionState;
+enum systemOption{None, GenerateWaves, MixWaves, PlayUnmixed, PlayMixed};
 enum inputState{OK, NOT_OK};
+
+//External Variable
+extern UART_HandleTypeDef huart1;
+extern enum systemOption optionState;
 
 //Function Declarations
 void userInterfaceWelcome(void);//UI function to show welcome message 
 void userInterfaceOptions(void);//UI function to input option state
-void userInterfaceGenerateSignal();//UI function to input signal frequency 
+void userInterfaceGenerateSignal(void);//UI function to input signal frequency 
 void userInterfaceMixer(void);//UI function to input matrix coefficients
-int isCorrect();//Function to verify input with the user
+int isCorrect(void);//Function to verify input with the user
 void generateSineWave(void);//Generates two signals based on the frequency given
 void signalMixer(void);//// Generates a mixed signal based on the two generated signals and given linear coefficients
 
